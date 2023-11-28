8656a4a2a2018b9a0d0675875e354067017afa39
Henryp2995 <60760419+Henryp2995@users.noreply.github.com>
2023-11-28 16:38:53 -0500

    second step

--- lab.js
+++ lab.js
@@ -1,14 +1,31 @@
-let x = 1
-while (x < 101){
+// let x = 1
+// while (x < 101){
     
-    if (x % 3 === 0){
-    console.log("Buzz")}
-    if (x % 5 === 0){
-        console.log("Fizz")}
-    if (x % 3 === 0 && x % 5 === 0){
-            console.log("Fizz Buzz")}
-    else if (x % 3 !== 0 && x % 5 !== 0){
-        console.log(x)
+//     if (x % 3 === 0){
+//     console.log("Buzz")}
+//     if (x % 5 === 0){
+//         console.log("Fizz")}
+//     if (x % 3 === 0 && x % 5 === 0){
+//             console.log("Fizz Buzz")}
+//     if (x % 3 !== 0 && x % 5 !== 0){
+//         console.log(x)
+//     }
+//     x++
+// }
+let n = 3
+while (n < 51)
+ {
+    let Prime = true;
+    let divisor = 2;
+     while (divisor <= Math.sqrt(n)) {
+        if (n % divisor === 0) {
+            Prime = false;
+            break;
+        }
+        divisor++;
     }
-    x++
+        if (Prime) {
+        console.log(n + " Prime");
+    }
+     n++;
 }
